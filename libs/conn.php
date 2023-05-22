d6d8ee4069ce7fb669517c79efed23054f0d911c
negritoxxx <124810034+negritoxxx@users.noreply.github.com>
2023-05-21 21:32:55 -0500

    Update conn.php

--- libs/conn.php
+++ libs/conn.php
@@ -16,7 +16,7 @@
 	    
 	    	$options = array(
 		    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',	
-		    PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/BaltimoreCyberTrustRoot.crt.pem',
+		    PDO::MYSQL_ATTR_SSL_KEY => 'BaltimoreCyberTrustRoot.crt.pem',
 		    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
 		);
 
