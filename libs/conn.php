f7f11e791ab1811b7f37997e6983891e71d98352
negritoxxx <124810034+negritoxxx@users.noreply.github.com>
2023-05-21 21:22:01 -0500

    Update conn.php

--- libs/conn.php
+++ libs/conn.php
@@ -13,6 +13,12 @@
 
 		$sql = "mysql:host=$servername;dbname=$database;";
 		$dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+	    
+	    	$options = array(
+		    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',	
+		    PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/BaltimoreCyberTrustRoot.crt.pem',
+		    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+		);
 
 	    	$my_Db_Connection = new PDO($sql, $username, $password, $dsn_Options);
 		return $my_Db_Connection;
